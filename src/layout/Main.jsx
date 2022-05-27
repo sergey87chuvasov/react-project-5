7a8475e568ff5e5f509cd98c0de5224d0ddbaa9f
Sergey87 <serge.techno@gmail.com>
2022-05-28 00:58:52 +0300

    added movie list

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,30 @@
-function Main () {
-    return <main className="container content" >
-        Hello from Movies App
-    </main>
+import React from 'react';
+import Movies from '../components/Movies';
+
+class Main extends React.Component {
+  state = {
+    movies: [],
+  };
+
+  componentDidMount() {
+    fetch('http://www.omdbapi.com/?apikey=fec1a62a&s=matrix')
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <main className='container content'>
+        {movies.length ? (
+          <Movies movies={this.state.movies} />
+        ) : (
+          <h3>Loading...</h3>
+        )}
+      </main>
+    );
+  }
 }
 
-export default Main
+export default Main;
