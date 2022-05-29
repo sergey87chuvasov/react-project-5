2f27fe3e98a20685d252ef301d38a4b83b4affcb
Sergey87 <serge.techno@gmail.com>
2022-05-29 23:51:42 +0300

    added search and logics

---
+++ src/components/Search.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class Search extends React.Component {
+  state = {
+    search: '',
+  };
+
+  handleKey = (event) => {
+    if (event.key === 'Enter') {
+      this.props.searchMovies(this.state.search);
+    }
+  };
+
+  render() {
+    return (
+      <div className='row'>
+        <div className='col s12'>
+          <div className='input-field'>
+            <input
+              placeholder='search'
+              type='search'
+              className='validate'
+              value={this.state.search}
+              onChange={(e) => this.setState({ search: e.target.value })}
+              onKeyDown={this.handleKey}
+            />
+            <button
+              className='btn search-btn'
+              onClick={() => this.props.searchMovies(this.state.search)}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Search;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Movies from '../components/Movies';
+import Preloader from '../components/Preloader';
+import Search from '../components/Search';
 
 class Main extends React.Component {
   state = {
@@ -12,16 +14,20 @@ class Main extends React.Component {
       .then((data) => this.setState({ movies: data.Search }));
   }
 
+  // функция по обновлен state и спущенная вниз
+  searchMovies = (str) => {
+    fetch(`http://www.omdbapi.com/?apikey=fec1a62a&s=${str}`)
+      .then((response) => response.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
+
   render() {
     const { movies } = this.state;
 
     return (
       <main className='container content'>
-        {movies.length ? (
-          <Movies movies={this.state.movies} />
-        ) : (
-          <h3>Loading...</h3>
-        )}
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}
       </main>
     );
   }
